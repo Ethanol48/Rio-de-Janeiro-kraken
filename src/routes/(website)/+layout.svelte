e4f4f26f30deb8823181b3976024bfd28ba91c6c
Ethan Rouimi <contact@ethan-rouimi.com>
2025-02-01 18:13:05 +0100

    add icon when signed in

--- src/routes/(website)/+layout.svelte
+++ src/routes/(website)/+layout.svelte
@@ -1,24 +1,30 @@
 <script lang="ts">
-  import Button from '$lib/components/ui/button/button.svelte';
-  import type { LayoutServerData } from './$types';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import type { LayoutServerData } from './$types';
+	import userIcon from '$lib/svgs/userIcon.svg';
 
-	let { data, children }: { data: LayoutServerData, children: any } = $props();
-
+	let { data, children }: { data: LayoutServerData; children: any } = $props();
 </script>
 
-
-{#if data.user === null}
-  
-
-  <Button class="absolute top-5 right-5" size="lg" href="/login">Log in</Button>
-
-
-{:else}
-  
-  <Button class="absolute top-3 right-3" href="/signout">Sign out</Button>
-  <!-- else content here -->
-
-
-{/if}
+<div class="absolute right-7 top-7 flex h-fit w-fit">
+	{#if data.user === null}
+		<Button size="lg" href="/login">Log in</Button>
+	{:else}
+		<div>
+			<div class="flex flex-col justify-center">
+				<div class="mb-2 rounded bg-primary p-4 text-center text-white">
+					<div class="flex flex-row">
+						<img src={userIcon} class="mr-2 h-6 w-6" alt="" />
+						<div>
+							{data.user.login}
+						</div>
+					</div>
+				</div>
+				<Button size="lg" href="/signout">Sign out</Button>
+			</div>
+		</div>
+	{/if}
+</div>
+<!-- else content here -->
 
 {@render children()}
