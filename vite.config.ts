b1c50aad2d9ff790ba35080f84f2799ceaf3d772
Ethan Rouimi <contact@ethan-rouimi.com>
2025-02-08 00:26:18 +0100

    fix bug of vite for node-rs

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,21 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  optimizeDeps: {
+    exclude: ["@node-rs/argon2", "@node-rs/bcrypt"]
+  },
 	plugins: [sveltekit()],
 	server: {
 		fs: {
 			allow: ['public']
-		  },
-		proxy: {
-		  '/api': {
-			target: 'http://localhost:3000', // URL de votre serveur backend
-			changeOrigin: true,
-			secure: false,
-		  },
 		},
-	  },
+
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000', // URL de votre serveur backend
+				changeOrigin: true,
+				secure: false
+			}
+		}
+	}
 });
