7b8c80e824a095f7e16bdfd66fadc9f5728285f7
Armand-MS <armand.ms.6280@gmail.com>
2025-03-19 12:54:38 +0100

    change confetti dep

---
+++ src/routes/(website)/games/enigme/+page.svelte
@@ -0,0 +1,275 @@
+<script>
+    import { onMount } from 'svelte';
+    import confetti from 'canvas-confetti';
+    let userInput = '';
+    let message = '';
+    const correctAnswer = 'PA'; // Remplace par la réponse correcte
+  
+    function checkAnswer() {
+      if (userInput.toLowerCase().trim() === correctAnswer.toLowerCase()) {
+        message = '✅ Bonne réponse !';
+        launchConfetti();
+      } else {
+        message = '❌ Mauvaise réponse, réessayez.';
+      }
+    }
+  
+    function launchConfetti() {
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+      });
+    }
+  </script>
+  
+  <div class="container">
+    <h1 class="enigme-title">🎭 Énigme du jour</h1>
+    <div class="enigme-box">
+      <p class="enigme-text">Quel est le plus gros skill issue d'EPITA ?</p>
+    </div>
+    <div class="input-container">
+      <!-- Input stylisé (inspiré de Uiverse.io par AbanoubMagdy1) -->
+      <div class="wave-group">
+        <input required type="text" class="input" bind:value={userInput} >
+        <span class="bar"></span>
+        <label class="label">
+          <span class="label-char" style="--index: 0">L</span>
+          <span class="label-char" style="--index: 1">e</span>
+          <span class="label-char ml-1" style="--index: 3">c</span>
+          <span class="label-char" style="--index: 4">o</span>
+          <span class="label-char" style="--index: 5">d</span>
+          <span class="label-char" style="--index: 6">e</span>
+        </label>
+      </div>
+      <!-- Bouton stylisé (inspiré de Uiverse.io par adamgiebl) -->
+      <button on:click={checkAnswer}>
+        <div class="svg-wrapper-1">
+          <div class="svg-wrapper">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+              <path fill="none" d="M0 0h24v24H0z"></path>
+              <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
+            </svg>
+          </div>
+        </div>
+        <span>Valider</span>
+      </button>
+    </div>
+    <p class="message">{message}</p>
+  </div>
+  
+  <style>
+    @keyframes slideUnderline {
+      from { width: 0; }
+      to { width: 100%; }
+    }
+    
+    @keyframes fadeIn {
+      0% { opacity: 0; transform: translateY(-20px); }
+      100% { opacity: 1; transform: translateY(0); }
+    }
+  
+    .container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      min-height: 100vh;
+      text-align: center;
+      padding: 20px;
+    }
+    .enigme-title {
+      font-size: 2.5rem;
+      font-weight: bold;
+      color: var(--background);
+      margin-bottom: 20px;
+      text-decoration: none;
+      position: relative;
+    }
+    .enigme-title::after {
+      content: '';
+      position: absolute;
+      left: 0;
+      bottom: -5px;
+      width: 100%;
+      height: 4px;
+      background-color: #6c2a2a;
+      animation: slideUnderline 1s ease-in-out;
+    }
+    .enigme-box {
+      border: 2px solid #007BFF;
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+      margin: 20px 0;
+      animation: fadeIn 1s ease-in-out;
+      width: 100%;
+      max-width: 400px;
+    }
+    .enigme-text {
+      font-size: 1.5rem;
+      font-weight: bold;
+      color: #333;
+    }
+    .input-container {
+      display: flex;
+      gap: 10px;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      max-width: 400px;
+      margin-top: 20px;
+    }
+    /* Stylisation de la wave-group */
+    .wave-group { 
+      position: relative;
+      width: 100%;
+      max-width: 250px;
+    }
+    .wave-group .input {
+      font-size: 16px;
+      padding: 10px 10px 10px 5px;
+      display: block;
+      width: 100%;
+      border: none;
+      border-bottom: 1px solid #515151;
+      background: transparent;
+    }
+    .wave-group .input:focus {
+      outline: none;
+    }
+    .wave-group .label {
+      color: #999;
+      font-size: 18px;
+      font-weight: normal;
+      position: absolute;
+      pointer-events: none;
+      left: 5px;
+      top: 10px;
+      display: flex;
+    }
+    .wave-group .label-char {
+      transition: 0.2s ease all;
+      transition-delay: calc(var(--index) * 0.05s);
+    }
+    .wave-group .input:focus ~ .label .label-char,
+    .wave-group .input:valid ~ .label .label-char {
+      transform: translateY(-20px);
+      font-size: 14px;
+      color: #5264AE;
+    }
+    .wave-group .bar {
+      position: relative;
+      display: block;
+      width: 100%;
+    }
+    .wave-group .bar:before,
+    .wave-group .bar:after {
+      content: '';
+      height: 2px;
+      width: 0;
+      bottom: 1px;
+      position: absolute;
+      background: var(--background);
+      transition: 0.2s ease all;
+    }
+    .wave-group .bar:before {
+      left: 50%;
+    }
+    .wave-group .bar:after {
+      right: 50%;
+    }
+    .wave-group .input:focus ~ .bar:before,
+    .wave-group .input:focus ~ .bar:after {
+      width: 50%;
+    }
+  
+    /* Bouton (inspiré de Uiverse.io par adamgiebl) */
+    button {
+      font-family: inherit;
+      font-size: 20px;
+      background: royalblue;
+      color: white;
+      padding: 0.7em 1em;
+      padding-left: 0.9em;
+      display: flex;
+      align-items: center;
+      border: none;
+      border-radius: 16px;
+      overflow: hidden;
+      transition: all 0.2s;
+      cursor: pointer;
+    }
+    button span {
+      display: block;
+      margin-left: 0.3em;
+      transition: all 0.3s ease-in-out;
+    }
+    button svg {
+      display: block;
+      transform-origin: center center;
+      transition: transform 0.3s ease-in-out;
+    }
+    button:hover .svg-wrapper {
+      animation: fly-1 0.6s ease-in-out infinite alternate;
+    }
+    button:hover svg {
+      transform: translateX(1.2em) rotate(45deg) scale(1.1);
+    }
+    button:hover span {
+      transform: translateX(5em);
+    }
+    button:active {
+      transform: scale(0.95);
+    }
+    @keyframes fly-1 {
+      from { transform: translateY(0.1em); }
+      to { transform: translateY(-0.1em); }
+    }
+    .message {
+      margin-top: 15px;
+      font-size: 1.5rem;
+      font-weight: bold;
+      color: #333;
+      text-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
+    }
+  
+    /* Responsive styles */
+    @media (max-width: 667px) {
+      .enigme-title {
+        font-size: 1.8rem;
+        margin-bottom: 10px;
+      }
+      .enigme-box {
+        padding: 10px;
+        margin: 10px 0;
+      }
+      .enigme-text {
+        font-size: 1rem;
+      }
+      .input-container {
+        flex-direction: column;
+        gap: 15px;
+      }
+      .wave-group {
+        max-width: 90%;
+      }
+      
+    }
+    @media (max-width: 400px) {
+      .enigme-title {
+        font-size: 1.2rem;
+      }
+      .enigme-box {
+        padding: 8px;
+      }
+      .enigme-text {
+        font-size: 0.9rem;
+      }
+      .wave-group {
+        max-width: 100%;
+      }
+      
+    }
+  </style>
+  
