93eafc36fc7a992d8f47f8d36ea5e86d4455a448
Armand-MS <armand.ms.6280@gmail.com>
2025-03-21 20:01:09 +0100

    Backend du validator fini :)

--- src/routes/(website)/shop_commande/+page.server.ts
+++ src/routes/(website)/shop_commande/+page.server.ts
@@ -17,7 +17,6 @@ export const load: PageServerLoad = async (event) => {
   async function GetListResult(userid : string,itemid : string,claimed: boolean | null) {
     return [await getUsername(userid), await GetNameItem(itemid),1,claimed]
   }
-  console.log(ListOrder)
 
   /// Récuperer les users
 
@@ -32,45 +31,90 @@ export const load: PageServerLoad = async (event) => {
   /// ajouter les users avec leur commande
 
   function UserInList(user_id : string, list ){ 
-    let compt = 1
+    let result = false;
     list.forEach(element => {
       if(element[0] === user_id){
-        console.log(!true)
-        return true;
+        result = true;
+        return;
       }
-      compt++;
     });
-    console.log(!false)
-    return false;
+    return result;
   }
+  
 
-  function UserHaveNotItem(item : string, list){
+  function AddItem( list,item : string){
+    let result = false
     list.forEach(element => {
-      if(item==element)
-        return false;
+      if(item==element[0])
+        {
+        element[1]++
+        result = true;
+        return;
+        }
     });
-    return true;
+    if(!result){
+      list.push([item,1])
+    }
   }
 
   
   list_userid.forEach(el_UserId => {
     ListOrder.forEach(el_ListOrder => {
-      if(el_UserId==el_ListOrder.userid){
-        if(!(UserInList(el_UserId, list_result))){
+      
+      if(el_UserId===el_ListOrder.userid){
+        if(!(UserInList(el_ListOrder.userid, list_result))){
           list_result.push([el_ListOrder.userid, [[el_ListOrder.itemid , 1]]])
         }
         else{
+
+          list_result.forEach(el_ListResult => {
+            if(el_ListResult[0] === el_UserId){
+              AddItem(el_ListResult[1], el_ListOrder.itemid);
+
+              
+            }
+            
+          });
+          
+            
+          }
           
           
         }
-      }
+      })
     });
-  });
 
-  console.log(list_result)
+  
+
+  console.log("La fin ",list_result);
+
+  // remettre les noms de base
  
 
+  async function GetNameItem_fct(item : string){
+    return await GetNameItem(item)
+  }
 
+  async function GetNameUser_fct(Username : string){
+    const a = await getUsername(Username)
+    return a;
+  }
+  async function updateUserOrders(list_result) {
+    for (let i = 0; i < list_result.length; i++) {
+      for (let j = 0; j < list_result[i][1].length; j++) {
+        list_result[i][1][j][0] = await GetNameItem_fct(list_result[i][1][j][0]);
+      }
+      list_result[i][0] = await GetNameUser_fct(list_result[i][0]);
+    }
+    
+  }
+  console.log("finn")
+  await updateUserOrders(list_result);
+  console.log(list_result[0])
+
+
+
+  
   /*
   ListOrder.forEach(element => {
     if(list_userid.includes(element.userid) && ){
