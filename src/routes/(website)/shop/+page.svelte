2b964c55ce844e8812e56aa0382e8ffc4523dfe5
Ethan Rouimi <contact@ethan-rouimi.com>
2025-03-26 13:22:59 +0100

    prepare for refactor

--- src/routes/(website)/shop/+page.svelte
+++ src/routes/(website)/shop/+page.svelte
@@ -15,11 +15,11 @@
 
 <title>Krak'n Roses - Shop</title>
 <div class="flex flex-col gap-2 justify-center">
-	<div class="flex h-[20vw] flex-col justify-center gap-3 text-center">
-		<h1 class="typography mb-3 text-lg text-gray-900">SHOP</h1>
+	<div class="flex h-[18vw] flex-col justify-center gap-3 text-center">
+		<h1 class="typography text-lg text-gray-900">SHOP</h1>
 	</div>
 
-	<div class="flex gap-6  flex-wrap justify-center">
+	<div class="flex gap-6 flex-col md:flex-row justify-center">
 		{#each data.items as item}
 			<Item user={data.user} {item} />
 		{/each}
